3f487b2234f6823892c736e22e8a84a5c1a5b178
AntiiiMage <zhangxu.otis@gmail.com>
2017-01-14 19:29:55 +0800

    re-check in

---
+++ cucumber-report-practice/src/test/java/feature/salary/SalaryTest.java
@@ -0,0 +1,12 @@
+package feature.salary;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = { "pretty", "json:target/cucumber.json" })
+public class SalaryTest {
+
+}
